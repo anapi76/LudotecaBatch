702d8df740589c5dbf425aa9abb801500c695caa
anapi76 <abpj76@gmail.com>
2024-12-13 09:55:35 +0100

    Inicio proyecto

---
+++ src/main/java/com/ccsw/tutorial_batch/TutorialBatchApplication.java
@@ -0,0 +1,13 @@
+package com.ccsw.tutorial_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccsw/tutorial_batch/TutorialBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccsw.tutorial_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
